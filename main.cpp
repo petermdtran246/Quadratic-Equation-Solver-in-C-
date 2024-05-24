0aa3d092c4cb30a15eda01b452e8f83aa8235b60
petermdtran246 <petermdtran246@gmail.com>
2024-05-24 17:43:48 +0700

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include<cmath>
+using namespace std;
+
+int main() {
+    float a,b,c,d,r1,r2;
+    cout << "Enter a,b,c: ";
+    cin >> a >>b >>c;
+    d= b*b-4*a*c;
+
+    if(d==0) {
+        cout << "Roots are real and equal" <<endl;
+        cout << (-b/(2*a));
+    } else if (d>0) {
+        cout << "Roots are real and unequal" << endl;
+        cout << (-b + sqrt(2 * a));
+        cout << (-b - sqrt(2 * a));
+    } else {
+        cout << "imaginary";
+    }
+    return 0;
+}
